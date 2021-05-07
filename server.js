10cc1192e6a3dc1e9bc24d5abd6267d2206298a9
muratcanoncu <murat.oncu1992@hotmail.com>
2021-05-07 11:17:11 +0200

    initial folder structure

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = 9000;
+
+app.use(express.static(`${__dirname}/public`));
+app.set("view engine", "hbs");
+
+//! Routes
+const indexRoute = require("./routes/indexRoute");
+//* index
+app.use("/", indexRoute);
+
+app.listen(PORT, () => {
+  console.log("Server is running on PORT::" + PORT);
+});
